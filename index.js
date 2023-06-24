0f3236f9a162a29b17d0c27ac4cf3161de8e992e
Riyad Agazade <riyadagazade81@gmail.com>
2023-06-24 21:46:36 +0400

    mathalgorithm

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+1  String
+2  Number
+3  Boolean
+4  Null
+5  Undefined
+6  Object
+7  Function
+*/
+
+
+if (10<3) {
+    console.log("Riyad duz deyir");
+}else if (10>3) {console.log("Isa duz deyir");}
+
